d24b1a3c737b3ee88f0299bf7ebf119459b117b0
Saksham <sakshamkumar1415@gmail.com>
2025-06-20 23:02:17 +0530

    feat: api version

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -29,13 +29,22 @@ func HandleConnection(conn net.Conn) {
 		receivedData := buffer[:n]
 		log.Printf("Received %d bytes from %s: %s", n, conn.RemoteAddr(), string(receivedData))
 
-		message_size := []byte{0x00, 0x00, 0x00, 0x00}
-		correlation_id := buffer[8:12]
-		bytesWritten, err := conn.Write(append(message_size, correlation_id...))
+		response := []byte{0x00, 0x00, 0x00, 0x00} // only message size initially
+		correlationId := buffer[8:12]
+		apiVersion := int(buffer[6])<<8 | int(buffer[7])
+
+		var errorCode byte = 0
+		if apiVersion < 0 || apiVersion > 4 {
+			errorCode = 35
+		}
+
+		response = append(response, correlationId...)
+		response = append(response, []byte{0x00, errorCode}...)
+
+		_, err = conn.Write(response)
 		if err != nil {
 			log.Printf("Error writing to %s: %v", conn.RemoteAddr(), err)
 			return
 		}
-		log.Printf("Sent %d bytes to %s", bytesWritten, bytesWritten)
 	}
 }
