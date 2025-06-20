318a7b03c1e5008049882402b3c0ebbd33a23c65
Saksham <sakshamkumar1415@gmail.com>
2025-06-20 23:29:21 +0530

    feat: response

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,11 +1,24 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 	"log"
 	"net"
 )
+
+func putInt16(val int16) []byte {
+	buf := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf, uint16(val))
+	return buf
+}
+
+func putInt32(val int32) []byte {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, uint32(val))
+	return buf
+}
 
 func HandleConnection(conn net.Conn) {
 	defer func() {
@@ -31,21 +44,33 @@ func HandleConnection(conn net.Conn) {
 		log.Printf("Received %d bytes from %s: %s", n, conn.RemoteAddr(), string(receivedData))
 
 		correlationId := buffer[8:12]
+		apiKey := binary.BigEndian.Uint16(buffer[4:6])
 		apiVersion := binary.BigEndian.Uint16(buffer[6:8])
 
-		var errorCode byte = 0
-		if apiVersion > 4 {
+		var errorCode int16 = 0
+		if apiVersion > 4 || apiKey != 18 {
 			errorCode = 35
 		}
 
-		response := []byte{0x00, 0x00, 0x00, 0x00} // only message size initially
-		response = append(response, correlationId...)
-		response = append(response, []byte{0x00, errorCode}...)
-		response = append(response, []byte{18}...)
-		response = append(response, []byte{0}...)
-		response = append(response, []byte{4}...)
+		response := new(bytes.Buffer)
+		response.Write(putInt16(int16(errorCode)))
+		response.WriteByte(0x00)
+		response.WriteByte(0x01)
+		response.Write(putInt16(18))
+		response.Write(putInt16(0))
+		response.Write(putInt16(4))
+		response.WriteByte(0x00)
+		response.WriteByte(0x01)
+		response.WriteByte(0x00)
 
-		_, err = conn.Write(response)
+		finalResponse := new(bytes.Buffer)
+		messageLength := int32(len(correlationId) + response.Len())
+		finalResponse.Write(putInt32(messageLength))
+
+		finalResponse.Write(correlationId)
+		finalResponse.Write(response.Bytes())
+
+		_, err = conn.Write(finalResponse.Bytes())
 		if err != nil {
 			log.Printf("Error writing to %s: %v", conn.RemoteAddr(), err)
 			return
